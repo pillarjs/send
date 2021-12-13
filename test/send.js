707070d4dbcf61a74b1b339e383807233bdac1e5
Douglas Christopher Wilson <doug@somethingdoug.com>
2021-12-12 22:15:02 -0500

    Remove deprecated index method

--- test/send.js
+++ test/send.js
@@ -815,44 +815,6 @@ describe('send(file).pipe(res)', function () {
     })
   })
 
-  describe('.index()', function () {
-    it('should be configurable', function (done) {
-      var app = http.createServer(function (req, res) {
-        send(req, req.url, { root: fixtures })
-          .index('tobi.html')
-          .pipe(res)
-      })
-
-      request(app)
-        .get('/')
-        .expect(200, '<p>tobi</p>', done)
-    })
-
-    it('should support disabling', function (done) {
-      var app = http.createServer(function (req, res) {
-        send(req, req.url, { root: fixtures })
-          .index(false)
-          .pipe(res)
-      })
-
-      request(app)
-        .get('/pets/')
-        .expect(403, done)
-    })
-
-    it('should support fallbacks', function (done) {
-      var app = http.createServer(function (req, res) {
-        send(req, req.url, { root: fixtures })
-          .index(['default.htm', 'index.html'])
-          .pipe(res)
-      })
-
-      request(app)
-        .get('/pets/')
-        .expect(200, fs.readFileSync(path.join(fixtures, 'pets', 'index.html'), 'utf8'), done)
-    })
-  })
-
   describe('.root()', function () {
     it('should set root', function (done) {
       var app = http.createServer(function (req, res) {
