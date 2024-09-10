ae4f2989491b392ae2ef3b0015a019770ae65d35
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2024-09-10 02:13:51 +0200

    Merge commit from fork
    Co-authored-by: Chris de Almeida <ctcpip@users.noreply.github.com>

--- test/send.js
+++ test/send.js
@@ -358,7 +358,7 @@ describe('send(file).pipe(res)', function () {
         .get('/pets')
         .expect('Location', '/pets/')
         .expect('Content-Type', /html/)
-        .expect(301, />Redirecting to <a href="\/pets\/">\/pets\/<\/a></, done)
+        .expect(301, />Redirecting to \/pets\/</, done)
     })
 
     it('should respond with default Content-Security-Policy', function (done) {
@@ -386,7 +386,7 @@ describe('send(file).pipe(res)', function () {
         .get('/snow')
         .expect('Location', '/snow%20%E2%98%83/')
         .expect('Content-Type', /html/)
-        .expect(301, />Redirecting to <a href="\/snow%20%E2%98%83\/">\/snow%20%E2%98%83\/<\/a></, done)
+        .expect(301, />Redirecting to \/snow%20%E2%98%83\/</, done)
     })
   })
 
